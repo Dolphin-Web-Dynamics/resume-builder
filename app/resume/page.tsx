a1cac006e9c310e4807a3f2fdeb7f77dab4fbdfa
Anel Canto <contact@anelcanto.com>
2025-02-19 09:40:34 -0700

    Add auth

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,12 +3,16 @@
 'use client';
 
 import ResumeDisplay from '../../components/ResumeDisplay';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 const ResumePage: React.FC = () => {
     return (
-        <div>
-            <ResumeDisplay />
-        </div>
+        <Authenticator>
+            <div>
+                <ResumeDisplay />
+            </div>
+        </Authenticator>
     );
 };
 
